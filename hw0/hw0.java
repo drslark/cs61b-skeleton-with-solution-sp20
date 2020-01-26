3423805b3fe23898d7ea4f02e8f186634cb9137c
Amit Bhat <amit.bhat@berkeley.edu>
2020-01-26 06:02:32 -0800

    Completed 3SUM and 3SUM_DISTINCT

--- hw0/hw0.java
+++ hw0/hw0.java
@@ -26,11 +26,48 @@ public class hw0 {
 		return max;
 	}
 
+	public static boolean threeSum(int[] a){
+
+		for (int i = 0; i < a.length; i++){
+			for (int j = 0; j < a.length; j++){
+				for (int k = 0; k < a.length; k++){
+					if (a[i] + a[j] + a[k] == 0)
+						return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	public static boolean threeSum_Distinct(int[] a){
+
+		for (int i = 0; i < a.length; i++){
+			for (int j = 0; j < a.length; j++){
+				if (i == j)
+					continue;
+				for (int k = 0; k < a.length; k++){
+					if (i == k || j == k) {
+						continue;
+					}
+					else if (a[i] + a[j] + a[k] == 0)
+						return true;
+				}
+			}
+		}
+		return false;
+	}
+
+
 
 	public static void main(String[] args) {
 		
 		int[] x = {1,2,3,5,4,2};
 
-		System.out.println(max_for(x) + " " + max_while(x));
+		//System.out.println(max_for(x) + " " + max_while(x));
+
+		System.out.println(threeSum(x) + " " + threeSum(new int[]{8, 2, -1, 15}));
+		System.out.println(threeSum_Distinct(x) + " " + threeSum_Distinct(new int[]{8, 2, -1, 15}));
+
+
 	}
 }
