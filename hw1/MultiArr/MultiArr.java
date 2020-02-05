177b8d55fcf7545cd54c75f9483c7d71de17f33d
Amit Bhat <amit.bhat@berkeley.edu>
2020-02-05 15:05:06 -0800

    Completed version 1.1 of MultiArr class

--- hw1/MultiArr/MultiArr.java
+++ hw1/MultiArr/MultiArr.java
@@ -1,4 +1,6 @@
-/** Multidimensional array 
+import java.sql.SQLOutput;
+
+/** Multidimensional array
  *  @author Zoe Plaxco
  */
 
@@ -14,7 +16,13 @@ public class MultiArr {
     Columns: 4
     */
     public static void printRowAndCol(int[][] arr) {
-        //TODO: Your code here!
+        System.out.println("Rows: " + arr.length);
+        int cols = 0;
+        for (int[] x: arr) {
+            if (x.length > cols)
+                cols = x.length;
+        }
+        System.out.println("Columns: " + cols);
     } 
 
     /**
@@ -22,14 +30,29 @@ public class MultiArr {
     @return maximal value present anywhere in the 2d array
     */
     public static int maxValue(int[][] arr) {
-        //TODO: Your code here!
-        return 0;
+        int max = arr[0][0];
+        for (int i = 0; i < arr.length; i += 1) {
+            for (int j = 0; j < arr[i].length; j += 1) {
+                if (arr[i][j] > max)
+                    max = arr[i][j];
+            }
+        }
+        return max;
     }
 
     /**Return an array where each element is the sum of the 
     corresponding row of the 2d array*/
     public static int[] allRowSums(int[][] arr) {
-        //TODO: Your code here!!
-        return null;
+        int[] sum = new int[arr.length];
+        for (int i = 0; i < arr.length; i += 1) {
+            int rowSum = 0;
+            for (int j = 0; j < arr[i].length; j += 1) {
+                rowSum += arr[i][j];
+            }
+            sum[i] = rowSum;
+        }
+        return sum;
+
     }
+
 }
--- hw1/MultiArr/MultiArrTest.java
+++ hw1/MultiArr/MultiArrTest.java
@@ -5,12 +5,63 @@ public class MultiArrTest {
 
     @Test
     public void testMaxValue() {
-        //TODO: Your code here!
+        int[][] test1 = {
+                {1, 2, 3, 4},
+                {1, 5, 12, 7},
+                {2, 2, 9, 4},
+                {8, 10, 3, 9}
+        };
+        int[][] test2 = {
+                {-2},
+                {-5, -1, -9},
+                {-3, -4, -6},
+                {-12, -10, -6, -23}
+        };
+        int[][] test3 = {
+                {1, 2, 3},
+                {5, 6, 6, 17, 8},
+                {4, 5},
+                {9}
+        };
+        assertEquals(12, MultiArr.maxValue(test1));
+        assertEquals(-1, MultiArr.maxValue(test2));
+        assertEquals(17, MultiArr.maxValue(test3));
     }
 
     @Test
     public void testAllRowSums() {
-        //TODO: Your code here!
+        int[][] test1 = {
+                {1, 2, 3, 4},
+                {1, 5, 12, 7},
+                {2, 2, 9, 4},
+                {8, 10, 3, 9}
+        };
+        int[][] test2 = {
+                {-2},
+                {-5, -1, -9},
+                {-3, -4, -6},
+                {-12, -10, -6, -23}
+        };
+        int[][] test3 = {
+                {1, 2, 3},
+                {5, 6, 6, 17, 8},
+                {4, 5}
+        };
+        int[][] test4 = {
+                {-2},
+                {5, -1, -9},
+                {-3, 4, 7},
+                {-12, 10, -6, -23}
+        };
+        int[] result1 = {10, 25, 17, 30};
+        int[] result2 = {-2, -15, -13, -51};
+        int[] result3 = {6, 42, 9};
+        int[] result4 = {-2, -5, 8, -31};
+        assertArrayEquals(result1, MultiArr.allRowSums(test1));
+        assertArrayEquals(result2, MultiArr.allRowSums(test2));
+        assertArrayEquals(result3, MultiArr.allRowSums(test3));
+        assertArrayEquals(result4, MultiArr.allRowSums(test4));
+
     }
 
 
