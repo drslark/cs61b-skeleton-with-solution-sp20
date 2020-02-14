771fa726c4a10a1b1569826ceac1f18f1fd4669b
Amit Bhat <amit.bhat@berkeley.edu>
2020-02-13 16:45:06 -0800

    Completed version 1.1 of Arrays.java and corresponding tests

--- hw2/arrays/Arrays.java
+++ hw2/arrays/Arrays.java
@@ -6,7 +6,7 @@ package arrays;
 /** HW #2 */
 
 /** Array utilities.
- *  @author
+ *  @Amit Bhat
  */
 class Arrays {
 
@@ -14,16 +14,32 @@ class Arrays {
     /** Returns a new array consisting of the elements of A followed by the
      *  the elements of B. */
     static int[] catenate(int[] A, int[] B) {
-        /* *Replace this body with the solution. */
-        return null;
+        int[] newArr = new int[A.length + B.length];
+
+        for (int i = 0; i < A.length; i += 1) {
+            newArr[i] = A[i];
+        }
+        for (int i = 0; i < B.length; i += 1) {
+            newArr[i + A.length] = B[i];
+        }
+
+        return newArr;
     }
 
     /* C2. */
     /** Returns the array formed by removing LEN items from A,
      *  beginning with item #START. */
     static int[] remove(int[] A, int start, int len) {
-        /* *Replace this body with the solution. */
-        return null;
+        int[] newArr = new int[A.length - len];
+
+        int j = 0;
+        for (int i = 0; i < A.length; i += 1) {
+            if ( !(start <= i && i < start + len) ) {
+                newArr[j] = A[i];
+                j += 1;
+            }
+        }
+        return newArr;
     }
 
     /* C3. */
@@ -33,7 +49,33 @@ class Arrays {
      *  returns the three-element array
      *  {{1, 3, 7}, {5}, {4, 6, 9, 10}}. */
     static int[][] naturalRuns(int[] A) {
-        /* *Replace this body with the solution. */
-        return null;
+        int runs = 0;
+        for (int i = 1; i < A.length; i += 1) {
+            if (A[i] <= A[i - 1])
+                runs += 1;
+            if (i + 1 == A.length)
+                runs += 1;
+        }
+
+        int[][] newArr = new int[runs][1];
+
+        int last = A[0];
+        int k, j;
+        k = j = 0;
+        int lenRun = 1;
+
+        for (int i = 1; i < A.length; last = A[i], i += 1) {
+            if (A[i] > last) {
+                lenRun += 1;
+            } else {
+                newArr[j] = Utils.subarray(A, k, lenRun);
+                j += 1;
+                k += lenRun;
+                lenRun = 1;
+            }
+        }
+        newArr[j] = Utils.subarray(A, k, lenRun);
+
+        return newArr;
     }
 }
--- hw2/arrays/ArraysTest.java
+++ hw2/arrays/ArraysTest.java
@@ -1,15 +1,71 @@
 package arrays;
 
+import lists.IntList;
+import lists.IntListList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/** FIXME
- *  @author FIXME
+/** Tests for the Arrays class.
+ *  @Amit Bhat
  */
 
 public class ArraysTest {
-    /** FIXME
-     */
+
+    @Test
+    public void testCatenate() {
+
+        int[] a = new int[0];
+        int[] b = {1, 2, 3};
+        int[] c = {4, 5, 6, 7};
+        int[] d = {-3, -2, -1, 0};
+
+        int[] bc = {1, 2, 3, 4, 5, 6, 7};
+        int[] db = {-3, -2, -1, 0, 1, 2, 3};
+        int[] ca = {4, 5, 6, 7};
+        int[] ad = {-3, -2, -1, 0};
+
+        assertArrayEquals(bc, Arrays.catenate(b, c));
+        assertArrayEquals(db, Arrays.catenate(d, b));
+        assertArrayEquals(ca, Arrays.catenate(c, a));
+        assertArrayEquals(ad, Arrays.catenate(a, d));
+    }
+
+    @Test
+    public void testRemove() {
+
+        int[] a = {-4, 3, 4, 5, 8, 1, 9, 2, 7};
+        int[] b = {1, 2, 3};
+        int[] c = {4, 5, 6, 7};
+        int[] d = {-3, -2, -1, 0};
+
+        int[] a1 = {-4, 3, 4, 5, 7};
+        int[] b1 = {1, 2};
+        int[] c1 = {4, 7};
+        int[] d1 = new int[0];
+
+        assertArrayEquals(a1, Arrays.remove(a, 4, 4));
+        assertArrayEquals(b1, Arrays.remove(b, 2, 1));
+        assertArrayEquals(c1, Arrays.remove(c, 1, 2));
+        assertArrayEquals(d1, Arrays.remove(d, 0, 4));
+    }
+
+    @Test
+    public void testNaturalRuns() {
+
+        int[] a = new int[] { 1, 3, 4, 4, 5, 2, 9, 12 };
+        int[][] a1 = new int[][] { {1, 3, 4}, {4, 5}, {2, 9, 12} };
+
+        int[] b = new int[] { -1, 0, 3, -3, 5, 0, 9, 12 };
+        int[][] b1 =  new int[][] { {-1, 0, 3}, {-3, 5}, {0, 9, 12} };
+
+        int[] c = new int[] { -1, -4, 0, 3, 9, -3 };
+        int[][] c1 = new int[][] { {-1}, {-4, 0, 3, 9}, {-3} };
+
+        assertArrayEquals(a1, Arrays.naturalRuns(a));
+        assertArrayEquals(b1, Arrays.naturalRuns(b));
+        assertArrayEquals(c1, Arrays.naturalRuns(c));
+
+    }
 
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(ArraysTest.class));
