000130c8562164cd9611a393cc36937bbf557bde
Amit Bhat <amit.bhat@berkeley.edu>
2020-03-18 22:48:33 -0700

    Completed version 1.2 of BitExercise class

--- hw5/BitExercise.java
+++ hw5/BitExercise.java
@@ -19,7 +19,7 @@ public class BitExercise {
     * For example: 2, 32, and 8192 are powers of two.
     */
     public static boolean powerOfTwo(int x) {
-        return (x & (x - 1)) == 0;
+        return (x & (x - 1)) == 0 || x == 0;
     }
     
     /** Fill in the function below so that it returns 
