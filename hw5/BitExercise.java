5a9d9ba45ba545d6747a544c01ded4a682f17556
Amit Bhat <amit.bhat@berkeley.edu>
2020-03-18 23:50:18 -0700

    Edited BitExercise

--- hw5/BitExercise.java
+++ hw5/BitExercise.java
@@ -20,7 +20,9 @@ public class BitExercise {
     */
     public static boolean powerOfTwo(int x) {
         int y = x & (x - 1);
-        return (y == 0 || x == 0);
+        int z = ~x & (~x + 1);
+
+        return (y == 0 || z == 0 || x == 0);
     }
     
     /** Fill in the function below so that it returns 
