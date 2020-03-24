412058ebe0bd96ab2850c7908ee8fc1a7a7cb5ee
Amit Bhat <amit.bhat@berkeley.edu>
2020-03-23 23:03:23 -0700

    Completed version 1.5 of ECHashStringSet

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /** A set of String values.
  *  @author Amit Bhat
  */
 class ECHashStringSet implements StringSet {
-    private StringList[] bins;
+    private LinkedList<String>[] bins;
     private int binCount;
     private int numItems;
 
     private static class StringList extends ArrayList<String> {}
 
     ECHashStringSet() {
-        bins = new StringList[4];
+        bins = new LinkedList[5];
         for (int i = 0; i < bins.length; i += 1) {
-            bins[i] = new StringList();
+            bins[i] = new LinkedList<>();
         }
-        binCount = 4;
+        binCount = 5;
         numItems = 0;
     }
 
@@ -27,49 +28,44 @@ class ECHashStringSet implements StringSet {
             resize();
         }
         if (!contains(s)) {
-            int binIndex = (s.hashCode() & 0x7fffffff) % binCount;
-            if (bins[binIndex] == null) {
-                bins[binIndex] = new StringList();
-            }
+            int binIndex = hash(s);
             bins[binIndex].add(s);
             numItems += 1;
         }
     }
 
-    private void resize() {
-        int newBinCount = binCount * 5;
-        StringList[] newBins = new StringList[newBinCount];
-        for (int i = 0; i < newBins.length; i += 1) {
-            newBins[i] = new StringList();
-        }
-
-        for (StringList x : bins) {
-            for (String s : x) {
-                int binIndex = (s.hashCode() & 0x7fffffff) % binCount;
-                newBins[binIndex].add(s);
-            }
-        }
-        binCount = newBinCount;
-        bins = newBins;
+    public int hash(String s) {
+        return (s.hashCode() & 0x7fffffff) % binCount;
     }
 
+    private void resize() {
+        binCount = binCount * 5;
+        List<String> oldBins = asList();
+
+        bins = new LinkedList[binCount];
+        for (int i = 0; i < bins.length; i += 1) {
+            bins[i] = new LinkedList<>();
+        }
+
+        for (String s : oldBins) {
+            int binIndex = hash(s);
+            bins[binIndex].add(s);
+        }
+    }
 
     @Override
     public boolean contains(String s) {
-        boolean contain = false;
-        for (StringList x : bins) {
-            if (x.contains(s)) {
-                contain = true;
-                break;
-            }
+        boolean contained = false;
+        if (bins[hash(s)] != null && bins[hash(s)].contains(s)) {
+            contained = true;
         }
-        return contain;
+        return contained;
     }
 
     @Override
     public List<String> asList() {
         ArrayList<String> allStrings = new ArrayList<String>();
-        for (StringList x : bins) {
+        for (LinkedList x : bins) {
             allStrings.addAll(x);
         }
         return allStrings;
