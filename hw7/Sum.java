d843a82ff18cb109122ea9a37c697cf55014196a
Amit Bhat <amit.bhat@berkeley.edu>
2020-04-10 22:52:53 -0700

    Completed version 1.1 of Sum class

--- hw7/Sum.java
+++ hw7/Sum.java
@@ -1,11 +1,24 @@
+import java.util.HashMap;
+
 /** HW #7, Two-sum problem.
- * @author
+ * @author Amit Bhat
  */
 public class Sum {
 
     /** Returns true iff A[i]+B[j] = M for some i and j. */
     public static boolean sumsTo(int[] A, int[] B, int m) {
-        return false;  // REPLACE WITH YOUR ANSWER
+        HashMap<Integer, Boolean> dict = new HashMap<Integer, Boolean>();
+
+        for (int x : B) {
+            dict.put(x, true);
+        }
+
+        for (int x : A) {
+            if (dict.containsKey(m - x)) {
+                return dict.get(m-x);
+            }
+        }
+        return false;
     }
 
 }
